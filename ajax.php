71d8f0d5fb4a681cf3f901e0148c1b4571ab2a3a
github-eakmsa <eakmsa@gmail.com>
2021-08-17 08:20:40 +0300

    edit and delete events manage multiple calendars' events

---
+++ ajax.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+header('Content-type: application/json');
+
+require_once('google-calendar-api.php');
+
+try {
+	// Get event details
+	$event = $_POST['event_details'];
+
+	// get calendar id
+	$calendar = $_POST['calendar'];
+
+	$capi = new GoogleCalendarApi();
+
+	switch($event['operation']) {
+		case 'create':
+			// Get user calendar timezone
+			if(!isset($_SESSION['user_timezone']))
+				$_SESSION['user_timezone'] = $capi->GetUserCalendarTimezone($_SESSION['access_token']);
+
+			// Create event on primary calendar
+			$event_id = $capi->CreateCalendarEvent($calendar, $event['title'], $event['all_day'], $event['event_time'], $_SESSION['user_timezone'], $_SESSION['access_token']);
+
+			echo json_encode([ 'event_id' => $event_id ]);
+			break;
+
+		case 'update':
+			// Update event on primary calendar
+			$capi->UpdateCalendarEvent($event['event_id'], $calendar, $event['title'], $event['all_day'], $event['event_time'], $_SESSION['user_timezone'], $_SESSION['access_token']);
+
+			echo json_encode([ 'updated' => 1 ]);
+			break;
+
+		case 'delete':
+			// Delete event on primary calendar
+			$capi->DeleteCalendarEvent($event['event_id'], $calendar, $_SESSION['access_token']);
+
+			echo json_encode([ 'deleted' => 1 ]);
+			break;
+	}
+}
+catch(Exception $e) {
+	header('Bad Request', true, 400);
+    echo json_encode(array( 'error' => 1, 'message' => $e->getMessage() ));
+}
+
+?>
